665a8749d52325648a1a14ba75b8e3e204e1121a
goobar <mike@slidetorial.com>
2018-01-20 21:33:56 +0100

    Initialize project template

---
+++ quotes-finder/src/main/java/com/slidetorial/qf/QuotesFinderApplication.java
@@ -0,0 +1,12 @@
+package com.slidetorial.qf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuotesFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuotesFinderApplication.class, args);
+	}
+}
---
+++ quotes-finder/src/test/java/com/slidetorial/qf/QuotesFinderApplicationTests.java
@@ -0,0 +1,16 @@
+package com.slidetorial.qf;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class QuotesFinderApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
